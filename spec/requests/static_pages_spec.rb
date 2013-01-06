eaddf7e608d0b373ee88fefbe17389ffe6c10dd3
Rajan Punchouty <rajan@ezzie.in>
2013-01-07 00:27:46 +0530

    Refractored test cases and layout file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Training App" }
+  
   describe "Home page" do
     it "should work" do
       get static_pages_home_path
@@ -15,20 +17,24 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Home')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Training App | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "| Home")
     end
   end
   
   describe "Help page" do
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', :text => "Help")
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
   
@@ -39,7 +45,7 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 end
