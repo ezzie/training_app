d4799dc2acc78c59ceeec6287240464a51b197ea
Rajan Punchouty <rajan@ezzie.in>
2013-01-06 23:18:15 +0530

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+    it "should work" do
+      get static_pages_home_path
+      response.status.should be(200)
+    end
+    it "should have the content 'Training App'" do
+      visit '/static_pages/home'
+      page.should have_content('Training App')
+    end
+    it "should have the h1 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Home')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Training App | Home")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Help")
+    end
+  end
+  
+  describe "About page" do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "About Us")
+    end
+  end
+end
